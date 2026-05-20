5ea09715b4bd50b412d2d7ca1c1e0019f0202eeb
Zero <228181855+Zero-Fetique@users.noreply.github.com>
2026-05-20 20:15:52 +0500

    Delete js directory

--- js/script.js
+++
@@ -1,2 +0,0 @@
-window.addEventListener('load', function () {setTimeout(function () {const preloader = document.querySelector('.preloader');preloader.classList.add('hidden-preloader');}, 1500);});
-
